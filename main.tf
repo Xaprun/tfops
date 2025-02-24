85696846a572eb224720cb2eee74ee9ee683ddaf
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-24 21:12:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  client_id       = jsondecode(base64decode(var.azure_credentials)).clientId
+  client_secret   = jsondecode(base64decode(var.azure_credentials)).clientSecret
+  subscription_id = jsondecode(base64decode(var.azure_credentials)).subscriptionId
+  tenant_id       = jsondecode(base64decode(var.azure_credentials)).tenantId
 }
 
 module "aks_spot" {
