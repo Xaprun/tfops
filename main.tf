ee376d775c176476192bbd7e54f80ca713546775
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-25 13:10:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,39 @@
-provider "azurerm" {
-  features {}
-  client_id       = jsondecode(base64decode(var.azure_credentials)).clientId
-  client_secret   = jsondecode(base64decode(var.azure_credentials)).clientSecret
-  subscription_id = jsondecode(base64decode(var.azure_credentials)).subscriptionId
-  tenant_id       = jsondecode(base64decode(var.azure_credentials)).tenantId
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.64.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "itsajtfstatestorage"
+    container_name       = "tfstate"          
+    key                  = "terraform.tfstate"
+  }
 }
 
-resource "azurerm_resource_group" "aks_rg" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azurerm" {
+  features {}
+  # client_id       = jsondecode(base64decode(var.azure_credentials)).clientId
+  # client_secret   = jsondecode(base64decode(var.azure_credentials)).clientSecret
+  # subscription_id = jsondecode(base64decode(var.azure_credentials)).subscriptionId
+  # tenant_id       = jsondecode(base64decode(var.azure_credentials)).tenantId
+}
+
+module "aks_spot" {
+  source = "git::https://github.com/Xaprun/tfmodules.git//aks/aks_spot"
+
+  aks_cluster_name          = var.aks_cluster_name
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  node_count                = var.node_count
+  node_vm_size              = var.node_vm_size
+  enable_additional_pool    = var.enable_additional_pool
+  additional_pool_mode      = var.additional_pool_mode
+  additional_pool_name      = var.additional_pool_name
+  additional_pool_vm_size   = var.additional_pool_vm_size
+  additional_pool_node_count = var.additional_pool_node_count
+  additional_pool_min_count  = var.additional_pool_min_count
+  additional_pool_max_count  = var.additional_pool_max_count
 }
