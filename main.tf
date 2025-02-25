7b90b3a4da90d6f8d8cfc602126e157e4f899632
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-02-25 06:50:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,19 +6,7 @@ provider "azurerm" {
   tenant_id       = jsondecode(base64decode(var.azure_credentials)).tenantId
 }
 
-module "aks_spot" {
-  source = "git::https://github.com/Xaprun/tfmodules.git//aks/aks_spot"
-
-  aks_cluster_name          = var.aks_cluster_name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  node_count                = var.node_count
-  node_vm_size              = var.node_vm_size
-  enable_additional_pool    = var.enable_additional_pool
-  additional_pool_mode      = var.additional_pool_mode
-  additional_pool_name      = var.additional_pool_name
-  additional_pool_vm_size   = var.additional_pool_vm_size
-  additional_pool_node_count = var.additional_pool_node_count
-  additional_pool_min_count  = var.additional_pool_min_count
-  additional_pool_max_count  = var.additional_pool_max_count
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
