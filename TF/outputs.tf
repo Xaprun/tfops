7e5921ebca62c51625eb1f898d7a407ec6eac1bb
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-12-29 22:32:25 +0100

    Update outputs.tf

--- TF/outputs.tf
+++ TF/outputs.tf
@@ -1,12 +1,12 @@
 output "aks_name" {
-  value = module.aks["dev"].aks_name
+  value = module.aks.aks_name
 }
 
 output "aks_fqdn" {
-  value = module.aks["dev"].aks_fqdn
+  value = module.aks.aks_fqdn
 }
 
 output "kube_config_raw" {
-  value     = module.aks["dev"].kube_config_raw
+  value     = module.aks.kube_config_raw
   sensitive = true
 }
