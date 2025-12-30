7a710a4f7434d42aecc9e10b85c9efca21f6bcfb
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-12-30 17:01:06 +0100

    Create grafana.tf [skip ci]

---
+++ TF/grafana.tf
@@ -0,0 +1,18 @@
+# Azure Managed Grafana
+resource "azurerm_dashboard_grafana" "this" {
+  name                = coalesce(var.grafana_name, "${var.aks_cluster_name}-grafana")
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# RBAC dla Grafany (MUSI BYĆ)
+resource "azurerm_role_assignment" "grafana_monitor_reader" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.this.identity[0].principal_id
+}
+
